1045f92a7e263a6748c01208daebaa68f0b446c9
Ashn <midvalue@gmail.com>
2017-10-06 06:26:18 -0400

    assignment 3 finish

--- js/main.js
+++ js/main.js
@@ -1,26 +1,63 @@
+/*********************************************************************************
+* WEB422 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Youngmin Kim Student ID: 109-161-158 Date: 2017-10-06
+*
+********************************************************************************/
+
 var viewModel = {
-  teams: ko.observable([]),
-  employees: ko.observable([]),
-  projects:ko.observable([])
+  teams: ko.observableArray([]),
+  employees: ko.observableArray([]),
+  projects:ko.observableArray([]),
+  saveTeam : function()
+  {  }
 };
+
+saveTeam = function()
+{
+  var currentTeam = this;
+  console.log(ko.mapping.toJS(currentTeam));
+  $.ajax({
+    url: "https://guarded-scrubland-73383.herokuapp.com/team/"+ko.mapping.toJS(currentTeam._id),
+    type: "PUT",
+    data: JSON.stringify({ Projects: ko.mapping.toJS(currentTeam.Projects),Employees: ko.mapping.toJS(currentTeam.Employees),TeamLead: ko.mapping.toJS(currentTeam.TeamLead) }), // we can also send data using the "data" option
+    contentType: "application/json"
+  })
+  .done(function () {
+    showGenericModal("Success","\"["+ko.mapping.toJS(currentTeam.TeamName) + "] Updated Successfully\", where ["+ko.mapping.toJS(currentTeam.TeamName)+"] is the \"TeamName\" of \"currentTeam\"");
+  }).fail(function (err)
+  {
+    showGenericModal("Error",err);
+  });
+}
+
+
 
 $(function(){
   console.log("jQuery is working.");
-  return new Promise((resolve,reject)=>{
+  //return new Promise((resolve,reject)=>{
     initializeTeams()
-    .then(initializeEmployees())
-    .then(initializeProjects())
+    .then(initializeEmployees)
+    .then(initializeProjects)
     .then(()=>{
       ko.applyBindings(viewModel);
-      $("#select, .multiple").multipleSelect({filter:true});
-      $("#select, .single").multipleSelect({single:true, filter:true});
+      //ko.applyBindings(saveTeam);
+      console.log(ko.mapping.toJS(viewModel));
+      $("select.multiple").multipleSelect({ filter: true });
+      $("select.single").multipleSelect({ single: true, filter: true });
+      console.log(ko.mapping.toJS(viewModel));
+      //resolve();
     }).catch((err)=>{
       showGenericModal("Error",err);
+      //reject();
     });
-  });
+  //});
 
 
-  function showGenericModal(title, message)
+  window.showGenericModal= function(title, message)
   {
     $(".modal-title").empty();
     $(".modal-body").empty();
@@ -41,6 +78,8 @@ $(function(){
       })
       .done(function (value) {
         viewModel.teams = ko.mapping.fromJS(value);
+        //console.log(value);
+
         resolve();
       }).fail(function (err)
       {
